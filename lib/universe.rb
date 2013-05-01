40ff940f9349cdf40109eede9efee5d1c066ea75
chrismo+☈king <chrismo+rking@sharpsaw.org>
2013-05-01 17:05:21 -0500

    Feedback to diminish lag
    From @chrismo and @tenderl♥ve both, so that sounds like the quorum
    has voted.

--- lib/universe.rb
+++ lib/universe.rb
@@ -1,6 +1,6 @@
 class Universe
   def initialize
-    lag = 2
+    lag = 1.5
     warn 'Doing stuff'; sleep 1 * lag
     warn 'Doing other stuff'; sleep 2 * lag
     warn 'Doing unnecessary stuff'; sleep 3 * lag
