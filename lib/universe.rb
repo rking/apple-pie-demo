ffcc1de85c7a4e36ce488ea4fb8766a647e77d30
amiel+☈king <amiel+rking@sharpsaw.org>
2013-05-01 15:51:06 -0500

    Spelling fix. Thanks web searching!

--- lib/universe.rb
+++ lib/universe.rb
@@ -3,7 +3,7 @@ class Universe
     lag = 2
     warn 'Doing stuff'; sleep 1 * lag
     warn 'Doing other stuff'; sleep 2 * lag
-    warn 'Doing unneccessary stuff'; sleep 3 * lag
+    warn 'Doing unnecessary stuff'; sleep 3 * lag
   end
 
   :SDG
